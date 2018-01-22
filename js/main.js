fa1f0860d62e8509ddaccd5d11b9028101101968
Daniel Graf <grafmark59@gmail.com>
2018-01-22 16:28:56 -0500

    added header section and navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
-})
+
+
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 50) {
+    $('.navbar').css('background-color','red');
+  } else {
+    $('.navbar').css('background-color','transparent');
+  }
+});
